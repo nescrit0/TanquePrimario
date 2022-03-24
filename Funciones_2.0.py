fa3f85b691fb5cab856cec3934001a2bbf77b199
nescrit0 <101613450+nescrit0@users.noreply.github.com>
2022-03-23 19:38:10 -0600

    Actualizacion

---
+++ Funciones_2.0.py
@@ -0,0 +1,4 @@
+def coches(*args):
+	print("El primer coche es: " + args[0] + " y el ultimo es"+ args[3] + '.')
+
+coches("RAV4", "CR-V", "Spark LT", "Versa")
---
+++ Funciones_3.0.py
@@ -0,0 +1,4 @@
+def letras (**kwargs):
+	print("Esta es la letra: " + kwargs["letra1"] + ".")
+
+letras(letra1="A", letra2="Z")
